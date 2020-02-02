f63b809bd0b796c5d413e83eb3f11ccda552382f
Othmane Zenzamy <59285391+DevopsZEN@users.noreply.github.com>
2020-02-03 00:19:37 +0100

    Add template to the custom component

--- TP/TP-1/main.js
+++ TP/TP-1/main.js
@@ -2,7 +2,15 @@ class AppNavbar extends HTMLElement{
     constructor(){
         super();
         this.attachShadow({mode:"open"});
-        this.shadowRoot.innerHTML = `<h2>Hello World!</h2>`;
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        console.log("Element added to the DOM");
+    }
+
+    disconnectedCallback() {
+        console.log("Element removed from the DOM");
     }
 }
 
