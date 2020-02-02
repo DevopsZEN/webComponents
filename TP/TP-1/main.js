0b9e80a217e0b163197b4c85e94ef98c8306ae73
Glitch (devopszen-webcomponents) <none>
2020-02-02 23:32:54 +0000

    🚌🚣 Updated with Glitch

--- TP/TP-1/main.js
+++ TP/TP-1/main.js
@@ -1,18 +1,27 @@
-class AppNavbar extends HTMLElement{
-    constructor(){
-        super();
-        this.attachShadow({mode:"open"});
-        const template = document.querySelector("template");
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-    }
+class AppNavbar extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    const template = document.querySelector("template");
+    console.log(this.getAttribute("theme"));
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+  
+  static get observedAttributes(){
+    return ["theme"];
+  }
 
-    connectedCallback() {
-        console.log("Element added to the DOM");
-    }
+  attributeChangedCallback(name, oldVal, newVal) {
+    console.table({ name, oldVal, newVal });
+  }
 
-    disconnectedCallback() {
-        console.log("Element removed from the DOM");
-    }
+  connectedCallback() {
+    console.log("Element added to the DOM");
+  }
+
+  disconnectedCallback() {
+    console.log("Element removed from the DOM");
+  }
 }
 
 window.customElements.define("app-navbar", AppNavbar);
